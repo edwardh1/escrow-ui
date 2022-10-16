cd4b183994f17913848ef2b84b174b71598d855d
edwardh <edwardharsono91@gmail.com>
2022-10-16 10:42:33 +0800

    edited

--- src/util/initEscrow.ts
+++ src/util/initEscrow.ts
@@ -1,4 +1,4 @@
-import { AccountLayout, Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+// import { AccountLayout, Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { Account, Connection, PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY, Transaction, TransactionInstruction } from "@solana/web3.js";
 import BN from "bn.js";
 import { ESCROW_ACCOUNT_DATA_LAYOUT, EscrowLayout } from "./layout";
@@ -6,34 +6,43 @@ import { ESCROW_ACCOUNT_DATA_LAYOUT, EscrowLayout } from "./layout";
 const connection = new Connection("http://localhost:8899", 'singleGossip');
 
 export const initEscrow = async (
-    privateKeyByteArray: string,
-    initializerXTokenAccountPubkeyString: string,
-    amountXTokensToSendToEscrow: number,
-    initializerReceivingTokenAccountPubkeyString: string,
-    expectedAmount: number,
-    escrowProgramIdString: string) => {
-    const initializerXTokenAccountPubkey = new PublicKey(initializerXTokenAccountPubkeyString);
-
-    //@ts-expect-error
-    const XTokenMintAccountPubkey = new PublicKey((await connection.getParsedAccountInfo(initializerXTokenAccountPubkey, 'singleGossip')).value!.data.parsed.info.mint);
-
+    // privateKeyByteArray: string,
+    // initializerTokenAccountPubkeyString: string,
+    // amountXTokensToSendToEscrow: number,
+    // receiverAccountPubkeyString: string,
+    // playerAAccountPubkeyString: string, // this account should already have the desired lamports
+    // playerBAccountPubkeyString: string, // this account should already have the desired lamports
+    // initializerReceivingTokenAccountPubkeyString: string,
+    // expectedAmount: number,
+    // escrowProgramIdString: string) => {
+    ) => {
+    const expectedAmount = 0.5;
+    const initializerTokenAccountPubkey = new PublicKey("CGiSu9eXwivVvPs6ebTHab12UfiP2RUdS2C8QaBNjViE");
+    
+    // const XTokenMintAccountPubkey = new PublicKey((await connection.getParsedAccountInfo(initializerXTokenAccountPubkey, 'singleGossip')).value!.data.parsed.info.mint);
+    const privateKeyByteArray = "148,146,219,199,221,68,202,155,116,252,249,101,254,219,95,196,225,179,134,207,183,36,90,79,196,214,133,232,210,206,128,139,167,119,33,207,159,135,135,62,33,160,186,11,198,28,200,69,207,33,40,72,139,253,195,255,200,201,160,119,198,67,223,39";
     const privateKeyDecoded = privateKeyByteArray.split(',').map(s => parseInt(s));
     const initializerAccount = new Account(privateKeyDecoded);
 
-    const tempTokenAccount = new Account();
-    const createTempTokenAccountIx = SystemProgram.createAccount({
-        programId: TOKEN_PROGRAM_ID,
-        space: AccountLayout.span,
-        lamports: await connection.getMinimumBalanceForRentExemption(AccountLayout.span, 'singleGossip'),
-        fromPubkey: initializerAccount.publicKey,
-        newAccountPubkey: tempTokenAccount.publicKey
-    });
-    const initTempAccountIx = Token.createInitAccountInstruction(TOKEN_PROGRAM_ID, XTokenMintAccountPubkey, tempTokenAccount.publicKey, initializerAccount.publicKey);
-    const transferXTokensToTempAccIx = Token
-        .createTransferInstruction(TOKEN_PROGRAM_ID, initializerXTokenAccountPubkey, tempTokenAccount.publicKey, initializerAccount.publicKey, [], amountXTokensToSendToEscrow);
+    const playerAAccountPubkey  = new PublicKey("4vxyp9auWavnRrVmHyUpjyYXNJK8KTtpGSr9KmohZxGH");
+    const playerBAccountPubkey  = new PublicKey("5LntGETLcCX4h7BrqLhoRv7s6dDK1HppDY8in8XxuXTZ");
+    const receiverAccountPubkey  = new PublicKey("CGiSu9eXwivVvPs6ebTHab12UfiP2RUdS2C8QaBNjViE");
+
+    // const tempTokenAccount = new Account();
+    // const createTempTokenAccountIx = SystemProgram.createAccount({
+    //     programId: TOKEN_PROGRAM_ID,
+    //     space: AccountLayout.span,
+    //     lamports: await connection.getMinimumBalanceForRentExemption(AccountLayout.span, 'singleGossip'),
+    //     fromPubkey: initializerAccount.publicKey,
+    //     newAccountPubkey: tempTokenAccount.publicKey
+    // });
+    // const initTempAccountIx = Token.createInitAccountInstruction(TOKEN_PROGRAM_ID, XTokenMintAccountPubkey, tempTokenAccount.publicKey, initializerAccount.publicKey);
+    // const transferXTokensToTempAccIx = Token
+    //     .createTransferInstruction(TOKEN_PROGRAM_ID, initializerXTokenAccountPubkey, tempTokenAccount.publicKey, initializerAccount.publicKey, [], amountXTokensToSendToEscrow);
     
     const escrowAccount = new Account();
-    const escrowProgramId = new PublicKey(escrowProgramIdString);
+    const escrowProgramId = new PublicKey("B78rZxBkyhoCi9fcsEbGCcpgqt7QcMJ9JDXfmvqkYdeV");
+    // const escrowProgramId = new PublicKey(escrowProgramIdString);
 
     const createEscrowAccountIx = SystemProgram.createAccount({
         space: ESCROW_ACCOUNT_DATA_LAYOUT.span,
@@ -43,22 +52,36 @@ export const initEscrow = async (
         programId: escrowProgramId
     });
 
+    // const initEscrowIx = new TransactionInstruction({
+    //     programId: escrowProgramId,
+    //     keys: [
+    //         { pubkey: initializerAccount.publicKey, isSigner: true, isWritable: false },
+    //         { pubkey: tempTokenAccount.publicKey, isSigner: false, isWritable: true },
+    //         { pubkey: new PublicKey(initializerReceivingTokenAccountPubkeyString), isSigner: false, isWritable: false },
+    //         { pubkey: escrowAccount.publicKey, isSigner: false, isWritable: true },
+    //         { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false},
+    //         { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+    //     ],
+    //     data: Buffer.from(Uint8Array.of(0, ...new BN(expectedAmount).toArray("le", 8)))
+    // })
+
     const initEscrowIx = new TransactionInstruction({
         programId: escrowProgramId,
         keys: [
             { pubkey: initializerAccount.publicKey, isSigner: true, isWritable: false },
-            { pubkey: tempTokenAccount.publicKey, isSigner: false, isWritable: true },
-            { pubkey: new PublicKey(initializerReceivingTokenAccountPubkeyString), isSigner: false, isWritable: false },
+            { pubkey: playerAAccountPubkey, isSigner: false, isWritable: true },
+            { pubkey: playerBAccountPubkey, isSigner: false, isWritable: false },
+            { pubkey: receiverAccountPubkey, isSigner: false, isWritable: false },
             { pubkey: escrowAccount.publicKey, isSigner: false, isWritable: true },
             { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false},
-            { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
         ],
         data: Buffer.from(Uint8Array.of(0, ...new BN(expectedAmount).toArray("le", 8)))
     })
-
+    // const tx = new Transaction()
+    //     .add(createTempTokenAccountIx, initTempAccountIx, transferXTokensToTempAccIx, createEscrowAccountIx, initEscrowIx);
     const tx = new Transaction()
-        .add(createTempTokenAccountIx, initTempAccountIx, transferXTokensToTempAccIx, createEscrowAccountIx, initEscrowIx);
-    await connection.sendTransaction(tx, [initializerAccount, tempTokenAccount, escrowAccount], {skipPreflight: false, preflightCommitment: 'singleGossip'});
+        .add(createEscrowAccountIx, initEscrowIx);
+    await connection.sendTransaction(tx, [initializerAccount, escrowAccount], {skipPreflight: false, preflightCommitment: 'singleGossip'});
 
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
